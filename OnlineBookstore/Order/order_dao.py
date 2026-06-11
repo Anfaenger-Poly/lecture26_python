d3225cefa29ff2ad98c6bc02603c2956d87a0d8d
Anfaenger-Poly <seck999@gmail.com>
2026-06-11 19:03:55 +0900

    Add files via upload

--- OnlineBookstore/Order/order_dao.py
+++ OnlineBookstore/Order/order_dao.py
@@ -3,31 +3,31 @@ from Order.order import Order
 # 주문 데이터 접근 (CRUD) : OrderDAO
 class OrderDAO:
     def __init__(self):
-        self.__orderDB: dict[str, Order] = {}
+        self.__orderDB = {}
 
-    def insert_order(self, order: Order) -> bool:
+    def insert_order(self, order):
         order_id = order.get_order_id()
         if order_id in self.__orderDB:
             return False
         self.__orderDB[order_id] = order
         return True
 
-    def select_order_by_id(self, order_id: str) -> Order | None:
+    def select_order_by_id(self, order_id):
         return self.__orderDB.get(order_id)
 
-    def select_all_orders(self) -> list[Order]:
+    def select_all_orders(self):
         return list(self.__orderDB.values())
 
-    def select_orders_by_member(self, member_id: str) -> list[Order]:
+    def select_orders_by_member(self, member_id):
         return [o for o in self.__orderDB.values() if o.get_member_id() == member_id]
 
-    def update_order(self, order_id: str, order: Order) -> bool:
+    def update_order(self, order_id, order):
         if order_id in self.__orderDB:
             self.__orderDB[order_id] = order
             return True
         return False
 
-    def delete_order(self, order_id: str) -> bool:
+    def delete_order(self, order_id):
         if order_id in self.__orderDB:
             self.__orderDB.pop(order_id)
             return True
