27042782287b41517afcf18662ce7e6d069ce08f
Anfaenger-Poly <seck999@gmail.com>
2026-06-15 12:32:06 +0900

    Add files via upload

--- OnlineBookstore/Order/order_dao.py
+++ OnlineBookstore/Order/order_dao.py
@@ -1,15 +1,19 @@
 from Order.order import Order
+from Order.order_item_dao import OrderItemDAO
 
 # 주문 데이터 접근 (CRUD) : OrderDAO
 class OrderDAO:
     def __init__(self):
         self.__orderDB = {}
+        self.__order_item_dao = OrderItemDAO()
 
     def insert_order(self, order):
         order_id = order.get_order_id()
         if order_id in self.__orderDB:
             return False
         self.__orderDB[order_id] = order
+        for item in order.get_items():
+            self.__order_item_dao.insert_order_item(order_id, item)
         return True
 
     def select_order_by_id(self, order_id):
@@ -30,5 +34,6 @@ class OrderDAO:
     def delete_order(self, order_id):
         if order_id in self.__orderDB:
             self.__orderDB.pop(order_id)
+            self.__order_item_dao.delete_items_by_order_id(order_id)
             return True
         return False
---
+++ OnlineBookstore/Order/order_item_dao.py
@@ -0,0 +1,43 @@
+from Order.order_item import OrderItem
+
+class OrderItemDAO:
+    def __init__(self):
+        self.__orderItemDB = {}
+        self.__seq = 1
+
+    def insert_order_item(self, order_id, order_item):
+        order_item_id = str(self.__seq)
+        self.__seq += 1
+        self.__orderItemDB[order_item_id] = (order_id, order_item)
+        return order_item_id
+    
+    def select_order_items_by_order_id(self, order_id):
+        result = []
+        for oid, item in self.__orderItemDB.values():
+            if oid == order_id:
+                result.append(item)
+        return result
+
+    def select_all_order_items(self):
+        return list(self.__orderItemDB.values())
+    
+    def update_order_item(self, order_item_id, order_item):
+        if order_item_id in self.__orderItemDB:
+            order_id = self.__orderItemDB[order_item_id][0]
+            self.__orderItemDB[order_item_id] = (order_id, order_item)
+            return True
+        return False
+    
+    def delete_order_item(self, order_item_id):
+        if order_item_id in self.__orderItemDB:
+            self.__orderItemDB.pop(order_item_id)
+            return True
+        return False
+    
+    def delete_items_by_order_id(self, order_id):
+        keys_to_delete = []
+        for k, (oid, _) in self.__orderItemDB.items():
+            if oid == order_id:
+                keys_to_delete.append(k)
+        for k in keys_to_delete:
+            self.__orderItemDB.pop(k)
